c19b2748ee8f70453ae2703d056a59891a125e8d
wtoillier <wtoillier@outlook.com>
2021-03-10 12:17:46 -0300

    Add files via upload

---
+++ Pratica01/Ex02.hs
@@ -0,0 +1,3 @@
+{-Escreva uma função para calcular o dobro de um número-}
+
+dobro x = x * 2 
---
+++ Pratica01/Ex03.hs
@@ -0,0 +1,4 @@
+{-Escreva uma função para quadruplicar um número usando a função dobro definida no
+item anterior.-}
+dobro x = x * 2
+quadruplicar x = dobro((dobro x)) 
