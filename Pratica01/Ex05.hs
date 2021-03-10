df41e8d93b0a070a0f3658c71a9224d148b304b9
wtoillier <wtoillier@outlook.com>
2021-03-10 12:25:45 -0300

    Delete Ex05.hs

--- Pratica01/Ex05.hs
+++
@@ -1,6 +0,0 @@
-{-Escreva uma função para calcular a distância entre dois pontos A e B num plano
-cartesiano. -}
-{-Como não foi visto o assunto de vetores, não fiz o ponto como coordenads de x,y,z...-}
-
-distancia :: Float -> Float -> Float
-distancia a b = abs(b - a)
